c096a8831c5deadc3fa9e77ddda7838e0fbec340
Никита Морозов <rdtyworldd@MacBook-Air-Nikita.local>
2025-10-11 21:35:48 +0300

    added dao interface, action interface, realize TaskAction

---
+++ actions/task_actions.go
@@ -0,0 +1,34 @@
+package actions
+
+import (
+	"github.com/RdtyWorldd/go_task_tracker_cli/dao"
+)
+
+type action interface {
+	Do() (int, error)
+}
+
+type TaskAction struct {
+	args []string
+	dao  dao.Crud_dao
+}
+
+func NewTaskAction(_args []string, _dao dao.Crud_dao) TaskAction {
+	return TaskAction{_args, _dao}
+}
+
+func (act TaskAction) add() {
+
+}
+
+func (act TaskAction) update() {
+
+}
+
+func (act TaskAction) delete() {
+
+}
+
+func (act TaskAction) Do() (int, error) {
+	return 0, nil
+}
---
+++ dao/crud_dao.go
@@ -0,0 +1,8 @@
+package dao
+
+type Crud_dao interface {
+	Create()
+	Read()
+	Update()
+	Delete()
+}
