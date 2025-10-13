c26d0c066516aab7609748b17e47bf76c9560bd9
Никита Морозов <rdtyworldd@MacBook-Air-Nikita.local>
2025-10-13 18:20:48 +0300

    realized add, update, delete task actions

--- actions/task_actions.go
+++ actions/task_actions.go
@@ -1,7 +1,10 @@
 package actions
 
 import (
+	"time"
+
 	"github.com/RdtyWorldd/go_task_tracker_cli/dao"
+	"github.com/RdtyWorldd/go_task_tracker_cli/task"
 )
 
 type TaskAction struct {
@@ -13,16 +16,34 @@ func NewTaskAction(_args []string, _dao dao.Crud_dao) TaskAction {
 	return TaskAction{_args, _dao}
 }
 
-func (act TaskAction) add() {
+func (act TaskAction) add(desc string) error {
+	// if len(act.args) < 3 {
+	// 	return errors.New("there are too few arguments")
+	// }
+	tasks := act.dao.ReadAll()
+	id := len(tasks) + 1
+	status := task.TODO
+	created := time.Now()
+	updated := created
 
+	act.dao.Create(task.Task{id, desc, status, created, updated})
+	return nil
 }
 
-func (act TaskAction) update() {
-
+func (act TaskAction) update(id int, upd_desc string) error {
+	task, err := act.dao.Read(id)
+	if err != nil {
+		panic(err)
+	}
+	task.Description = upd_desc
+	task.UpdatedAt = time.Now()
+	act.dao.Update(id, task)
+	return nil
 }
 
-func (act TaskAction) delete() {
-
+func (act TaskAction) delete(id int) error {
+	err := act.dao.Delete(id)
+	return err
 }
 
 func (act TaskAction) Do() (int, error) {
