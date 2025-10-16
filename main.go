d1ae2fede07ae52809b0711864b1abc15b229c0f
Никита Морозов <rdtyworldd@MacBook-Air-Nikita.local>
2025-10-16 23:52:10 +0300

    Added the creation of a directory json to task list file

--- main.go
+++ main.go
@@ -13,6 +13,11 @@ func main() {
 	if len(args) <= 1 {
 		fmt.Println("Arguments incorect. \n Use -h flag to get help information")
 	}
+	_, err := os.Stat("./json")
+	if err != nil {
+		os.Mkdir("json", 0755)
+	}
+
 	var action actions.Action
 	if args[1] == actions.HELP {
 		action = actions.NewHelpAction(args[0])
@@ -23,7 +28,7 @@ func main() {
 		return
 	}
 	action = actions.NewTaskAction(args, file.NewFileDao("json/tasks.json"))
-	err := action.Do()
+	err = action.Do()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
